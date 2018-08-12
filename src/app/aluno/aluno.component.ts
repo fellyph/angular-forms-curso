58b8e8ab9658ea7157ee69a1b695b9edbf16e00e
fellyph <fellyph.cintra@gmail.com>
2018-08-12 18:51:50 +0100

    adding formgroup

--- src/app/aluno/aluno.component.ts
+++ src/app/aluno/aluno.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Aluno } from '../models/aluno.model';
+import { FormGroup , FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-aluno',
@@ -8,10 +9,22 @@ import { Aluno } from '../models/aluno.model';
 })
 export class AlunoComponent implements OnInit {
   public aluno: Aluno;
+  public alunoForm: FormGroup;
 
   constructor() {
-    this.aluno = new Aluno('', '', '', '', '', '');
   }
-  ngOnInit() { }
+  ngOnInit() {
+    this.aluno = new Aluno('', '', '', '', '', '');
+    this.alunoForm = new FormGroup({
+      nome: new FormControl(),
+      sobrenome: new FormControl(),
+      profissao: new FormControl(),
+      cidade: new FormControl(),
+      estado: new FormControl(),
+      email: new FormControl()
+    });
+
+
+  }
 
 }
